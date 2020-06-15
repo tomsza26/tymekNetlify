db9cb3a5d87de254eab955ae025e44d7b5e2f013
tomsza26 <tomsza27@gmail.com>
2020-06-15 23:57:20 +0200

    test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,5 @@ const React = require('react');
 const Layout = require('./src/components/Layout').default;
 
 exports.wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>;
+  return <Layout>{element}</Layout>;
 };
